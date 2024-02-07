2732b09b79652ff32748678f455724a2d007cb97
ViktorAgejev <155150698+ViktorAgejev@users.noreply.github.com>
2024-02-07 17:22:03 +0100

    napokon je card na kraju bijel ali opet nekako nije u sredini

--- app/src/main/java/com/example/qgen/AIgeneriranje.kt
+++ app/src/main/java/com/example/qgen/AIgeneriranje.kt
@@ -1,20 +1,14 @@
 package com.example.qgen
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -22,9 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.alexstyl.swipeablecard.ExperimentalSwipeableCardApi
 import com.alexstyl.swipeablecard.rememberSwipeableCardState
@@ -33,53 +25,62 @@ import com.alexstyl.swipeablecard.swipableCard
 @OptIn(ExperimentalSwipeableCardApi::class)
 @Composable
 fun AIgeneriranje(navigiranjeEkrana: NavHostController) {
-    val state = rememberSwipeableCardState()
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(
-                start = 38.dp
-            )
-            .swipableCard(
-                state = state,
-                onSwiped = { direction ->
-                    println("The card was swiped to $direction")
-                },
-                onSwipeCancel = {
-                    println("The swiping was cancelled")
-                }
-            )
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Card {
-            Text("Pitanje")
-            OutlinedTextField(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                value = "BlaBlaBla",
-                onValueChange = { /*TODO*/ }
-            )
-            Text("Odgovor 1")
-            PitanjeRow()
-            Text("Odgovor 2")
-            PitanjeRow()
-            Text("Odgovor 3")
-            PitanjeRow()
-            Text("Pitanje")
-            OutlinedTextField(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                value = "BlaBlaBla",
-                onValueChange = { /*TODO*/ }
-            )
+        val state = rememberSwipeableCardState()
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    start = 38.dp
+                )
+                .swipableCard(
+                    state = state,
+                    onSwiped = { direction ->
+                        println("The card was swiped to $direction")
+                    },
+                    onSwipeCancel = {
+                        println("The swiping was cancelled")
+                    }
+                )
+        ) {
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White
+                )
+            ) {
+                    Text("Pitanje")
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        value = "BlaBlaBla",
+                        onValueChange = { /*TODO*/ }
+                    )
+                    Text("Odgovor 1")
+                    PitanjeRow()
+                    Text("Odgovor 2")
+                    PitanjeRow()
+                    Text("Odgovor 3")
+                    PitanjeRow()
+                    Text("Pitanje")
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        value = "BlaBlaBla",
+                        onValueChange = { /*TODO*/ }
+                    )
 
+                }
+
+            }
         }
-
     }
-}
-@Composable
-fun PitanjeRow(){
-    Row {
-        OutlinedTextField(
+    @Composable
+    fun PitanjeRow() {
+        Row {
+            OutlinedTextField(
             modifier = Modifier
                 .padding(start = 24.dp),
             value = "blablabla",
