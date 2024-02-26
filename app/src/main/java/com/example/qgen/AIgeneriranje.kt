421f4528d90dce37913cc264494c24ffab60d73a
ViktorAgejev <155150698+ViktorAgejev@users.noreply.github.com>
2024-02-26 08:04:53 +0100

    sad ima ikona za aplikaciju umjesto one zelene od android studia, pojavi se crveni tekst ako je lozinka kriva, i kad se klikne na kod se može shareati

--- app/src/main/java/com/example/qgen/AIgeneriranje.kt
+++ app/src/main/java/com/example/qgen/AIgeneriranje.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
@@ -208,3 +207,4 @@ Row(
     )
 }
 }
+//a
